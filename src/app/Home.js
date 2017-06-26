313cb28bc23c5021b074237134c7d99e8a90df18
Asante Buil <Asante Buil>
2017-06-26 09:31:32 -0500

    added toolbar and icons Signed-off-by: Asante Buil <Asante Buil>

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import  React from 'react'
 
 import {BrowserRouter as Router, Route, Link, NavLink, Redirect} from 'react-router-dom'
 
@@ -13,39 +13,15 @@ import {observer} from 'mobx-react'
 import { Scrollbars } from 'react-custom-scrollbars'
 import Particles from 'react-particles-js'
 import particleConfig from './particles.json'
+import {iSearch, iTwit, iGit} from './icons.js'
 
 var dev_mode = false
 var DOMAIN_URL = 'https://yamochat.im'
 if(dev_mode){
   DOMAIN_URL = 'http://127.0.0.1:9001'
 }
-const values = [
-  'thrills',
-  'excitement',
-  'lunacy',
-  'geekiness',
-  'joy',
-  'innovation',
-  'lols',
-  'education',
-  'insanity',
-  '🍣',
-  'craftmanship',
-  '@#$!',
-  'chaos',
-  'splendor',
-  'madness',
-]
 
-const projects = [
-  {title: 'YamoChat', link: 'https://yamochat.im'},
-  {title: 'PGP-Chat', link: 'https://github.com/asantebuil/pgp-chat'},
-  {title: 'YamoBot', link: 'https://github.com/asantebuil/YamoBot'},
-  {title: 'GoFundMe- Scraper', link: 'https://github.com/asantebuil/gofundme-scraper'},
-  {title: 'Hearthstone Trivia Bot', link: 'https://github.com/asantebuil/hearthstone-trivia-bot'},
-  {title: 'WordScramble-JAVA', link: 'https://github.com/asantebuil/WordScramble'},
-  {title: 'WebRTC-Mafkr', link: 'https://github.com/asantebuil/WebRTC-Mafkr'}
-]
+
 
 @observer
 export default class Home extends React.Component {
@@ -54,24 +30,15 @@ export default class Home extends React.Component {
     super(props)
 
     this.state = {
-      rollingText: values[0],
-      index: 0,
-      projects: projects,
-      height: 32
+
     }
   }
 
   componentDidMount(){
-      this.randT()
-      this.timerID = setInterval(
-      ()=> this.randT(), 750
-    )
-        this.setState({height: document.getElementById('social').clientHeight})
 
   }
 
   componentWillUnMount(){
-    clearInterval(this.timerID)
   }
 
   randT = () =>{
@@ -83,39 +50,20 @@ export default class Home extends React.Component {
   }
 
   render() {
-    var height = this.state.height
     return (
-      <div id='container'>
-        <Particles params={particleConfig}/>
-          <div id='overlay'>
-            <div className="Header">
-              <img style={{maxHeight: '100%', maxWidth: '100%', zoom:'2'}} src='https://s3-us-west-2.amazonaws.com/geekenforcer.com/header_red.png'/>
-              <h4 style={{margin: '0px'}}>Creating things for the {this.state.rollingText}</h4>
-            </div>
-
-            <div className="HomeBody">
-              <Scrollbars universal>
-              <div style={{padding: '10px'}}>
-                <Button.Group color='red' vertical fluid>
-                  {
-                    this.state.projects.map((obj, i)=>{
-                      return(
-                        <Button key={i} color="red" onClick={()=>window.location.assign(obj.link)}>{obj.title}</Button>
-                      )
-                    })
-                  }
-                </Button.Group>
-                </div>
-              </Scrollbars>
-            </div>
-            <div className="Social" id='social'>
-              <div  style={{margin: '0 auto', height: '100%'}} >
-                <Image href='https://github.com/asantebuil' style={{padding:'3px'}} width='40' height='40' src='https://s3-us-west-2.amazonaws.com/geekenforcer.com/github.png'/>
-                <Image href='https://twitter.com/yamoshotto' style={{padding:'3px'}} width='40' height='40' src='https://s3-us-west-2.amazonaws.com/geekenforcer.com/twitter.png'/>
-              <a style={{fontSize: '100%', float: 'right'}} href="mailto:asante@geekenforcer.com">asante@geekenforcer.com</a>
-              </div>
-            </div>
-          </div>
+      <div className='container'>
+        <div className="toolbar">
+          <ul className='ToolBarElms'>
+            <li><a className="active" href="#home">Home</a></li>
+            <li><a href="#trending">Popular</a></li>
+            <li><a href="#about">About</a></li>
+            <li style={{float: 'right'}}><a href='#'><img src={iTwit}/></a></li>
+            <li style={{float: 'right'}} ><a href='#'><img src={iGit} /></a></li>
+            <li style={{float: 'right', padding: '12px'}}> <img src={iSearch} style={{paddingRight: '10px'}}/> <input type='text'></input></li>
+          </ul>
+        </div>
+        <div className="main">
+        </div>
       </div>
     )
   }
